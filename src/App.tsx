bce391a1e861341df724e3137f18c0d2557b3b5a
Joel <joel@gmail.com>
2026-01-18 18:03:47 +0530

    new

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
+import Jobs from "./pages/Jobs";
+import JobDetails from "./pages/JobDetails";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <div className="min-h-screen flex flex-col">
+          <Header />
+          <main className="flex-1">
+            <Routes>
+              <Route path="/" element={<Jobs />} />
+              <Route path="/jobs/:jobId" element={<JobDetails />} />
+              {/* Redirect any unknown routes to Home (Jobs) */}
+              <Route path="*" element={<Jobs />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
